8e954a72a632d030c368325fc729bc98ad70c37b
Alexey <yanshev.ak@gmail.com>
2025-12-19 15:28:24 +0500

    Version 1.0

--- backend/kittygram_backend/settings.py
+++ backend/kittygram_backend/settings.py
@@ -1,14 +1,17 @@
 # flake8: noqa
-import os
 from pathlib import Path
+
+from .config import settings
 
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-SECRET_KEY = 'django-insecure-cg6*%6d51ef8f#4!r3*$vmxm4)abgjw8mo!4y-q*uq1!4$-89$'
+SECRET_KEY = settings.django.SECRET_KEY.get_secret_value()
 
-DEBUG = True
+DEBUG = settings.django.DEBUG
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = settings.server.allowed_hosts
+
+CSRF_TRUSTED_ORIGINS = [f'https://{settings.server.DOMAIN}']
 
 INSTALLED_APPS = [
     'django.contrib.admin',
@@ -56,11 +59,14 @@ WSGI_APPLICATION = 'kittygram_backend.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': settings.db.NAME,
+        'USER': settings.db.USER,
+        'PASSWORD': settings.db.PASS.get_secret_value(),
+        'HOST': settings.db.HOST.get_secret_value(),
+        'PORT': settings.db.PORT,
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/3.2/ref/settings/#auth-password-validators
@@ -92,9 +98,10 @@ USE_TZ = True
 
 
 STATIC_URL = '/static/'
+STATIC_ROOT = BASE_DIR / 'collected_static'
 
 MEDIA_URL = '/media/'
-MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+MEDIA_ROOT = BASE_DIR / 'media'
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
@@ -111,3 +118,12 @@ REST_FRAMEWORK = {
     'PAGE_SIZE': 10,
 
 }
+
+# Cors
+
+CORS_ALLOWED_ORIGINS = [
+    f'https://{settings.server.DOMAIN}',
+    'http://localhost:3000',
+    'http://localhost:8000',
+    'http://127.0.0.1:3000',
+]
