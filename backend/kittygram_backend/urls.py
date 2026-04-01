3eb5939017e49a7b98bd8e0990f37764376a392c
Alexey <yanshev.ak@gmail.com>
2026-04-01 09:34:42 +0500

    refactor

--- backend/kittygram_backend/urls.py
+++ backend/kittygram_backend/urls.py
@@ -2,8 +2,8 @@ from django.conf import settings
 from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import include, path
-from rest_framework import routers
 from health_check.views import HealthCheckView
+from rest_framework import routers
 
 from cats.views import AchievementViewSet, CatViewSet
 from config import app_config
